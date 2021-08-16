624d5e28154d5b32563a5f1ee3bd5471b1deb54f
Andrew Larsen <ajlarsen2@gmail.com>
2021-08-16 11:53:20 -0400

    Updated database URI for deployment

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 // const Sequelize = require('sequelize');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/language-translator', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_ATLAS_URI || 'mongodb://localhost:27017/language-translator', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
