20ba8ed44a469015b6a0292808b90379120bbe7a
Andrew Larsen <ajlarsen2@gmail.com>
2021-08-23 15:01:59 -0400

    Remove console logs

--- server/db/db.js
+++ server/db/db.js
@@ -4,9 +4,5 @@ mongoose.connect(process.env.MONGODB_ATLAS_URI || 'mongodb://localhost:27017/lan
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Database Connected Successfully')
-
-})
 
 module.exports = db;
