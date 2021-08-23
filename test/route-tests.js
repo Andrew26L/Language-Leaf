9ab2ad5335c9960069b6c29f09a82c762d23857f
Andrew Larsen <ajlarsen2@gmail.com>
2021-08-23 15:02:17 -0400

    Add test specs for word routes

--- test/route-tests.js
+++ test/route-tests.js
@@ -7,18 +7,36 @@ describe("API Routes", () => {
   // beforeEach(async () => {
   //   await seed();
   // })
-  describe("API route for /api/sentences", () => {
-    it("Receives a response to GET request to /api/sentences", async() => {
+  describe("Word routes - /api/words", () => {
+    it("GET responds successfully with status code 200", async() => {
+      const res = await request(app).get("/api/words").expect(200)
+
+    })
+    it("GET responds with an array of word objects", async() => {
+      const res = await request(app).get("/api/words")
+      expect(res.body).to.be.an("array");
+      expect(res.body[0]).to.be.an("object");
+      expect(res.body.length).to.equal(4);
+    })
+    it("Each word object in the array contains an english property with an array of strings", async() => {
+      const res = await request(app).get("/api/words")
+      expect(res.body[0].english).to.be.an("array");
+      expect(res.body[0].english[0]).to.be.an("string");
+      expect(res.body[0].english[0].length).to.be.greaterThan(0);
+    })
+  })
+  describe("Sentence routes - /api/sentences", () => {
+    it("GET responds successfully with status code 200", async() => {
       const res = await request(app).get("/api/sentences").expect(200)
 
     })
-    it("API returns an array of four sentence objects", async() => {
+    it("GET responds with an array of sentence objects", async() => {
       const res = await request(app).get("/api/sentences")
       expect(res.body).to.be.an("array");
       expect(res.body[0]).to.be.an("object");
       expect(res.body.length).to.equal(4);
     })
-    it("Each sentence object contains an 'english' property with an array of sentences", async() => {
+    it("Each sentence object in the array contains an english property with an array of strings", async() => {
       const res = await request(app).get("/api/sentences")
       expect(res.body[0].english).to.be.an("array");
       expect(res.body[0].english[0]).to.be.an("string");
