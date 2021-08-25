46f73728816db2e5d7bd18544dd92b146667c059
Andrew Larsen <ajlarsen2@gmail.com>
2021-08-25 17:03:38 -0400

    Add new tests for mongoDB models

---
+++ test/model-test.js
@@ -0,0 +1,13 @@
+const { User } = require("../server/db");
+const { expect } = require("chai");
+const seed = require("../seed");
+
+describe("User Model", () => {
+  beforeEach(async () => {
+    const user = new User({
+      username: ""
+    })
+    currentWord.save(function(err, currentWord)
+  })
+  describe("")
+})
