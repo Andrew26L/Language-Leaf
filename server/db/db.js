4508d9ebaa42510dd6b0a89dc8a983fcc8c9945a
Andrew Larsen <ajlarsen2@gmail.com>
2021-08-25 17:02:05 -0400

    Configure separate test database connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,14 @@
 // const Sequelize = require('sequelize');
+
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_ATLAS_URI || 'mongodb://localhost:27017/language-translator', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_ATLAS_URI || process.env.NODE_ENV === 'test' ? 'mongodb://localhost:27017/language-translator-test' : 'mongodb://localhost:27017/language-translator', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log('Database Connected Successfully')
+  }
+})
 
 module.exports = db;
