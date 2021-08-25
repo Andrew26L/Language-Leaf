16ad8f5a26edd9ec0c8df4bfa280683a726c3e50
Andrew Larsen <ajlarsen2@gmail.com>
2021-08-25 17:23:59 -0400

    Add test spec for generateToken

--- test/model-test.js
+++ test/model-test.js
@@ -1,13 +1,22 @@
 const { User } = require("../server/db");
 const { expect } = require("chai");
-const seed = require("../seed");
+const jwt = require("jsonwebtoken")
 
 describe("User Model", () => {
   beforeEach(async () => {
     const user = new User({
-      username: ""
+      username: "Emma",
+      password: "PasswordTest@#$",
+      email: "test@test.com"
     })
-    currentWord.save(function(err, currentWord)
+    user.save()
   })
-  describe("")
+  describe("Authentication", () => {
+    it("Generates a token when correct credentials are entered", async () => {
+      const user = await User.findOne({ username: "Emma" })
+      const testToken = await user.generateToken();
+      const { id } = await jwt.verify(testToken, process.env.JWT);
+      expect(id).to.equal(user.id)
+    })
+  })
 })
