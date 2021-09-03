3a09f32cd8b67f45bf0a0f33f4a08842ce626c6a
Andrew Larsen <ajlarsen2@gmail.com>
2021-09-03 12:48:42 -0400

    Add thunk to update overall user score at end of quiz

--- client/store/score.js
+++ client/store/score.js
@@ -1,12 +1,25 @@
+import axios from 'axios';
 
 // Action Types (Contants)
 const INCREMENT_SCORE = "INCREMENT_SCORE"
+const UPDATE_TOTAL_SCORE = "UPDATE_TOTAL_SCORE"
 
 // Action Creators
 export const incrementScore = () => {
   return ({
     type: INCREMENT_SCORE
   })
+}
+
+export const updateUserScore = (score) => {
+  return async (dispatch) => {
+    const { data: newScore } = await axios.put(
+      'api/score',
+      {score},
+      {headers: {
+        authorization: window.localStorage.getItem("token")
+      }});
+  }
 }
 
 export default (state = 0, action) => {
