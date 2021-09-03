f7646fadf208a6a877f72064d577612099383856
Andrew Larsen <ajlarsen2@gmail.com>
2021-09-03 17:31:52 -0400

    Modify DB connection URI

--- seed.js
+++ seed.js
@@ -168,7 +168,11 @@ async function seed() {
 
 async function runSeed() {
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_ATLAS_URI || process.env.NODE_ENV === 'test' ? 'mongodb://localhost:27017/language-translator-test' : 'mongodb://localhost:27017/language-translator', {useNewUrlParser: true, useUnifiedTopology: true});
+if (process.env.MONGODB_ATLAS_URI) {
+  mongoose.connect(process.env.MONGODB_ATLAS_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+} else {
+  mongoose.connect(process.env.NODE_ENV === 'test' ? 'mongodb://localhost:27017/language-translator-test' : 'mongodb://localhost:27017/language-translator', {useNewUrlParser: true, useUnifiedTopology: true});
+}
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
