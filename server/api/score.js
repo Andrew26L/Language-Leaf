4581367963a196aac2c923664a1c0042d1fed484
Andrew Larsen <ajlarsen2@gmail.com>
2021-09-03 13:10:00 -0400

    Get current user score from databasee

--- server/api/score.js
+++ server/api/score.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
 const { User } = require('../db');
 
+router.get('/', async (req, res, next) => {
+  try {
+    const user = await User.findByToken(req.headers.authorization)
+    res.send({score: user.score});
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.put('/', async (req, res, next) => {
   try {
     const user = await User.findByToken(req.headers.authorization)
-    user.score = user.score + req.body.score
+    user.score = req.body.score
     await user.save()
     res.send({score: user.score});
   } catch (error) {
