2f2935ec97b6800ad649ef63ef8368ac8d966dcf
Andrew Larsen <ajlarsen2@gmail.com>
2021-09-16 22:43:34 -0400

    Modify user declaration in user model test spec

--- test/model-test.js
+++ test/model-test.js
@@ -3,17 +3,16 @@ const { expect } = require("chai");
 const jwt = require("jsonwebtoken")
 
 describe("User Model", () => {
+  let user;
   beforeEach(async () => {
-    const user = new User({
+    user = new User({
       username: "Emma",
       password: "PasswordTest@#$",
       email: "test@test.com"
     })
-    user.save()
   })
   describe("Authentication", () => {
     it("Generates a token when correct credentials are entered", async () => {
-      const user = await User.findOne({ username: "Emma" })
       const testToken = await user.generateToken();
       const { id } = await jwt.verify(testToken, process.env.JWT);
       expect(id).to.equal(user.id)
