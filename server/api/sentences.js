19aaaee0e9e1b4e7f5141b19efa6680f24d05d32
Andrew Larsen <ajlarsen2@gmail.com>
2021-09-22 15:42:40 -0400

    Remove unused API routes

--- server/api/sentences.js
+++ server/api/sentences.js
@@ -12,15 +12,16 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.put('/', async (req, res, next) => {
-  try {
-    const sentence = await Sentence.findById(req.body._id);
-    sentence[req.body.lang].push(req.body.guess);
-    await sentence.save();
-    res.send(sentence);
-  } catch (error) {
-    next(error);
-  }
-})
+// Replaced with report API
+// router.put('/', async (req, res, next) => {
+//   try {
+//     const sentence = await Sentence.findById(req.body._id);
+//     sentence[req.body.lang].push(req.body.guess);
+//     await sentence.save();
+//     res.send(sentence);
+//   } catch (error) {
+//     next(error);
+//   }
+// })
 
 module.exports = router;
--- server/api/words.js
+++ server/api/words.js
@@ -12,15 +12,16 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.put('/', async (req, res, next) => {
-  try {
-    const word = await Word.findById(req.body._id);
-    word[req.body.lang].push(req.body.guess);
-    await word.save();
-    res.send(word);
-  } catch (error) {
-    next(error);
-  }
-})
+// Replaced with Report API
+// router.put('/', async (req, res, next) => {
+//   try {
+//     const word = await Word.findById(req.body._id);
+//     word[req.body.lang].push(req.body.guess);
+//     await word.save();
+//     res.send(word);
+//   } catch (error) {
+//     next(error);
+//   }
+// })
 
 module.exports = router;
