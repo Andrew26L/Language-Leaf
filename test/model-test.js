a29bfe240326763865a0b41c16ab83a534d6f639
Andrew Larsen <ajlarsen2@gmail.com>
2021-09-26 21:45:31 -0400

    Separate model tests into categories

--- test/model-test.js
+++ test/model-test.js
@@ -28,44 +28,47 @@ describe("Sentence Model", () => {
       german: ["Wie geht es ihnen?"]
     })
   })
-  it("Generates an instance with an English translation", async () => {
-    expect(sentence.english[0]).to.be.a("string");
-    expect(sentence.english[0]).to.equal("How are you?")
-  })
-  it("Generates an instance with a German translation", async () => {
-    expect(sentence.german[0]).to.be.a("string");
-    expect(sentence.german[0]).to.equal("Wie geht es ihnen?")
-  })
-  it("Generates an empty array for reports", async () => {
-    expect(sentence.reports).to.be.an("array");
-    expect(sentence.reports.length).to.equal(0)
-  })
-  it("Reports can be added to the instance", async () => {
-    sentence.reports.push({
-      language: "german",
-      guess: "Wie gehts?"
+  describe("Translations", () => {
+    it("Generates an instance with an English translation", async () => {
+      expect(sentence.english[0]).to.be.a("string");
+      expect(sentence.english[0]).to.equal("How are you?")
     })
-    expect(sentence.reports[0].guess).to.equal("Wie gehts?")
-    expect(sentence.reports[0].language).to.equal("german")
-    expect(sentence.reports.length).to.equal(1)
-  })
-  it("Reports are added as objects", async () => {
-    sentence.reports.push({
-      language: "german",
-      guess: "Wie gehts?"
+    it("Generates an instance with a German translation", async () => {
+      expect(sentence.german[0]).to.be.a("string");
+      expect(sentence.german[0]).to.equal("Wie geht es ihnen?")
     })
-    expect(sentence.reports[0]).to.be.an("object")
   })
-  it(`Reports have "language" and "guess" properties`, async () => {
-    sentence.reports.push({
-      language: "german",
-      guess: "Wie gehts?"
+  describe("Reports", () => {
+    it("Generates an empty array for reports", async () => {
+      expect(sentence.reports).to.be.an("array");
+      expect(sentence.reports.length).to.equal(0)
     })
-    expect(sentence.reports[0].language).to.be.a("string")
-    expect(sentence.reports[0].guess).to.be.a("string")
+    it("Reports can be added to the instance", async () => {
+      sentence.reports.push({
+        language: "german",
+        guess: "Wie gehts?"
+      })
+      expect(sentence.reports[0].guess).to.equal("Wie gehts?")
+      expect(sentence.reports[0].language).to.equal("german")
+      expect(sentence.reports.length).to.equal(1)
+    })
+    it("Reports are added as objects", async () => {
+      sentence.reports.push({
+        language: "german",
+        guess: "Wie gehts?"
+      })
+      expect(sentence.reports[0]).to.be.an("object")
+    })
+    it(`Reports have "language" and "guess" properties`, async () => {
+      sentence.reports.push({
+        language: "german",
+        guess: "Wie gehts?"
+      })
+      expect(sentence.reports[0].language).to.be.a("string")
+      expect(sentence.reports[0].guess).to.be.a("string")
+    })
   })
 })
-
 describe("Word Model", () => {
   let word;
   beforeEach(async () => {
@@ -75,44 +78,49 @@ describe("Word Model", () => {
       type: "noun"
     })
   })
-  it("Generates an instance with an English translation", async () => {
-    expect(word.english[0]).to.be.a("string");
-    expect(word.english[0]).to.equal("Train")
-  })
-  it("Generates an instance with a German translation", async () => {
-    expect(word.german[0]).to.be.a("string");
-    expect(word.german[0]).to.equal("Bahn")
-  })
-  it("Generates an instance with a word type (i.e. noun, verb, adjective, etc.)", async () => {
-    expect(word.type).to.be.a("string");
-    expect(word.type).to.equal("noun")
-  })
-  it("Generates an empty array for reports", async () => {
-    expect(word.reports).to.be.an("array");
-    expect(word.reports.length).to.equal(0)
-  })
-  it("Reports can be added to the instance", async () => {
-    word.reports.push({
-      language: "english",
-      guess: "train"
+  describe("Translations", () => {
+
+    it("Generates an instance with an English translation", async () => {
+      expect(word.english[0]).to.be.a("string");
+      expect(word.english[0]).to.equal("Train")
     })
-    expect(word.reports[0].guess).to.equal("train")
-    expect(word.reports[0].language).to.equal("english")
-    expect(word.reports.length).to.equal(1)
-  })
-  it("Reports are added as objects", async () => {
-    word.reports.push({
-      language: "english",
-      guess: "train"
+    it("Generates an instance with a German translation", async () => {
+      expect(word.german[0]).to.be.a("string");
+      expect(word.german[0]).to.equal("Bahn")
     })
-    expect(word.reports[0]).to.be.an("object")
   })
-  it(`Reports have "language" and "guess" properties`, async () => {
-    word.reports.push({
-      language: "english",
-      guess: "train"
+  describe("Reports", () => {
+    it("Generates an instance with a word type (i.e. noun, verb, adjective, etc.)", async () => {
+      expect(word.type).to.be.a("string");
+      expect(word.type).to.equal("noun")
     })
-    expect(word.reports[0].language).to.be.a("string")
-    expect(word.reports[0].guess).to.be.a("string")
+    it("Generates an empty array for reports", async () => {
+      expect(word.reports).to.be.an("array");
+      expect(word.reports.length).to.equal(0)
+    })
+    it("Reports can be added to the instance", async () => {
+      word.reports.push({
+        language: "english",
+        guess: "train"
+      })
+      expect(word.reports[0].guess).to.equal("train")
+      expect(word.reports[0].language).to.equal("english")
+      expect(word.reports.length).to.equal(1)
+    })
+    it("Reports are added as objects", async () => {
+      word.reports.push({
+        language: "english",
+        guess: "train"
+      })
+      expect(word.reports[0]).to.be.an("object")
+    })
+    it(`Reports have "language" and "guess" properties`, async () => {
+      word.reports.push({
+        language: "english",
+        guess: "train"
+      })
+      expect(word.reports[0].language).to.be.a("string")
+      expect(word.reports[0].guess).to.be.a("string")
+    })
   })
 })
