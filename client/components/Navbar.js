614b587ce3012c0b566bcd116615ac1ae9c41cba
Andrew Larsen <ajlarsen2@gmail.com>
2021-10-08 12:03:35 -0400

    Background color and button spacing for navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,44 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store/auth';
-import { makeStyles } from '@material-ui/core'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button'
 
-const useStyles = makeStyles(theme => ({
-  green: {
-    backgroundColor: "#2b2e27"
-  },
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  }
-}));
-
 const Navbar = ({isLoggedIn, handleClick}) => {
-  const classes = useStyles();
   return (
     <AppBar position="static" style={{backgroundColor: "#2b2e27"}}>
-      <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
+      <Toolbar className="topBar">
+        <Typography variant="h6">
         🌱 Language Leaf
         </Typography>
         {isLoggedIn ? (
-          <div id="navDiv">
+          <div>
             <Button color="inherit" onClick={handleClick}>Logout</Button>
           </div>
         ) : (
-          <div id="navDiv">
+          <div>
             <Link to="/login" className="navButton">
             <Button color="inherit">Log In</Button>
             </Link>
